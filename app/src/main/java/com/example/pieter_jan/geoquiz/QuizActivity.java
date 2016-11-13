93dab8e191f6560d5af245b55622e563e69ef75c
PJ-VDB <pjvandenbrande@hotmail.com>
2016-11-13 22:56:25 +0100

    Chapter 3: Activity lifecycle

--- app/src/main/java/com/example/pieter_jan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/pieter_jan/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.pieter_jan.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,10 +34,16 @@ public class QuizActivity extends AppCompatActivity {
 
     // The index for the question
     private int mCurrentIndex = 0;
+    // Tag for Debugging
+    private static final String TAG = "QuizActivity";
+    // Key to save state across screen rotations
+    private static final String KEY_INDEX = "index";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Log a message
+        Log.d(TAG, "onCreate (Bundle) called");
         setContentView(R.layout.activity_quiz);
 
 
@@ -99,14 +106,58 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        // Check if there is a previous instance (from screen rotation)
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+
         updateQuestion();
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question); // setText method requires the resource id of the string
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
     }
 
     private void checkAnswer(boolean userPressedTrue){
