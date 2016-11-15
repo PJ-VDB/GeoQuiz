28b083cc19c789fbe9d8934f3fa2f205f1585be4
PJ-VDB <pjvandenbrande@hotmail.com>
2016-11-15 18:32:07 +0100

    Chapter 6: TextView that shows the API level

--- app/src/main/java/com/example/pieter_jan/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/pieter_jan/geoquiz/CheatActivity.java
@@ -1,11 +1,15 @@
 package com.example.pieter_jan.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,33 +29,64 @@ public class CheatActivity extends AppCompatActivity {
 
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
-    public static final String EXTRA_ANSWER_SHOWN ="com.bignerdranch.android.geoquiz.answer_shown";
+    public static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private TextView mShowAPILevel;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
-        
+
         // Retrieve the correct answer
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
-    mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              if (mAnswerIsTrue) {
-                        mAnswerTextView.setText(R.string.true_button);
-                    } else {
-                  mAnswerTextView.setText(R.string.false_button);
-              }
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
                 setAnswerShownResult(true);
+
+
+                // Create animation for the "show answer" button
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        // Set a TextView that shows the API level
+        mShowAPILevel = (TextView) findViewById(R.id.show_api_level);
+        // Create the string that should be put in
+        String APItext = "API level " + android.os.Build.VERSION.SDK_INT;
+        // Put the string in the textfield
+        mShowAPILevel.setText(APItext);
+
 
         if(savedInstanceState != null) {
             setAnswerShownResult(savedInstanceState.getBoolean(KEY_CHEATER, false));
